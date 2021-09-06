8bdfb11cb3fea3a0e45dd939fdc46bdceea1f670
Eugene Starshov <87355480+evstarshov@users.noreply.github.com>
2021-09-06 11:10:32 +0300

    Add files via upload

--- VKClient_EVSTARSHOV/VKClient_EVSTARSHOV/Controllers/FriendsTableViewController.swift
+++ VKClient_EVSTARSHOV/VKClient_EVSTARSHOV/Controllers/FriendsTableViewController.swift
@@ -51,4 +51,35 @@ class FriendsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         54.0
     }
+    
+    @IBAction func addFriend(_ sender: Any) {
+        let addName = UIAlertController(
+        title: "Добавить друга",
+        message: nil,
+        preferredStyle: .alert
+        )
+        addName.addTextField()
+        
+        func getName() -> String {
+        let submitName = UIAlertAction(title: "Ok", style: .default) { [unowned addName] _ in
+        let answerName = addName.textFields![0]
+            return answerName
+        }
+        addName.addAction(submitName)
+        present(addName, animated: true)
+        }
+        
+        print(getName())
+        let addGroup = UIAlertController(
+        title: "В какой он группе?",
+        message: nil,
+        preferredStyle: .alert
+        )
+        addGroup.addTextField()
+        let submitGroup = UIAlertAction(title: "Ok", style: .default)
+        addGroup.addAction(submitGroup)
+        
+        present(addGroup, animated: true)
+        //friendsArray.append(Friends(image: nil, name: getName, groups: submitGroup))
+    }
 }
